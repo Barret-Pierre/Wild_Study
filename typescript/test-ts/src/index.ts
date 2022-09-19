25a435cf961903fe174aaa4c8a70874861d15a5e
Barret-Pierre <80763730+Barret-Pierre@users.noreply.github.com>
2022-09-19 17:14:59 +0200

    Add files via upload

--- typescript/test-ts/src/index.ts
+++ typescript/test-ts/src/index.ts
@@ -3,6 +3,7 @@
 interface IUser {
     name: string
     age: number
+    birthday?: string
 }
 const prettyPrintWilder = (users: IUser[]) :void => {
     users.map((user) => {
@@ -12,7 +13,7 @@ const prettyPrintWilder = (users: IUser[]) :void => {
   
   const wilders: IUser[] = [];
   const user1: IUser = { name: "Pierre", age: 23 };
-  const user2: IUser = { name: "Paul", age: 32};
+  const user2: IUser = { name: "Paul", age: 32, birthday: "10/02/1990"};
   const user3: IUser = { name: "Jacques", age: 25 };
   wilders.push(user1);
   wilders.push(user2);
